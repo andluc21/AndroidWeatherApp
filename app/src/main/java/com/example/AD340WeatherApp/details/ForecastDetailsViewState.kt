e1aa49a138fe497af4effc3f7df30a14337a601f
Costello <costello@Costellos-MacBook-Pro.local>
2023-08-15 10:44:27 -0700

    working

--- app/src/main/java/com/example/AD340WeatherApp/details/ForecastDetailsViewState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ad340weatherapp.details
-
-data class ForecastDetailsViewState (
-    val temp: Float,
-    val description: String,
-    val date: String,
-    val iconUrl: String
-)
